f722e7342378c8111b784ec7d88f9dace9ea1da4
hellzz13 <hpalenca13@gmail.com>
2022-02-12 09:36:11 -0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import InputSearch from "./components/InputSearch";
+
+function App() {
+  return (
+    <div className="App">
+      <InputSearch />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/InputSearch/index.tsx
@@ -0,0 +1,11 @@
+import react from  'react';
+
+const InputSearch = () => {
+    return(
+        <div>
+            <input type="text" />
+        </div>
+    )
+}
+
+export default InputSearch;
