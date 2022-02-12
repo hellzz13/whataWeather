15460ab98c1e11a3c75f4cea34e49d5b7e3fb180
hellzz13 <hpalenca13@gmail.com>
2022-02-12 10:21:25 -0300

    request inputSearch Api

--- src/components/InputSearch/index.tsx
+++ src/components/InputSearch/index.tsx
@@ -1,11 +1,24 @@
-import react from  'react';
+import react from "react";
 
 const InputSearch = () => {
-    return(
-        <div>
-            <input type="text" />
-        </div>
-    )
-}
+  const handleInputChange = (e: any) => {
+    e.preventDefault();
+    console.log(e.target.value);
+    const { value } = e.target;
+
+    if (!value) return;
+
+    const url = `http://wttr.in/${value}?format=j1`;
+    fetch(url)
+      .then((response) => response.json())
+      .then(console.log);
+  };
+
+  return (
+    <div>
+      <input type="text" onChange={handleInputChange} />
+    </div>
+  );
+};
 
 export default InputSearch;
