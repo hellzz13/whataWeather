1f3288819f1f8543d8fe47f8731768d9c912a1c8
hellzz13 <hpalenca13@gmail.com>
2022-02-13 09:35:25 -0300

    ajuste request

--- src/components/InputSearch/index.tsx
+++ src/components/InputSearch/index.tsx
@@ -1,6 +1,26 @@
-import react from "react";
+import react, { useState } from "react";
+
+type WeatherProps =
+  // {
+  // current_condition:[
+  {
+    FeelsLikeC: string;
+
+    nearest_area: [
+      {
+        region: [
+          {
+            value: string;
+          }
+        ];
+      }
+    ];
+  };
+// ]
+// }
 
 const InputSearch = () => {
+  const [region, setRegion] = useState<WeatherProps[]>([]);
   const handleInputChange = (e: any) => {
     e.preventDefault();
     console.log(e.target.value);
@@ -11,12 +31,14 @@ const InputSearch = () => {
     const url = `http://wttr.in/${value}?format=j1`;
     fetch(url)
       .then((response) => response.json())
-      .then(console.log);
+      .then((res) => setRegion(res.current_condition));
   };
 
+  console.log(region);
   return (
     <div>
       <input type="text" onChange={handleInputChange} />
+      <ul>{ region?.map((item) => <li>{item.FeelsLikeC}</li>)}</ul>
     </div>
   );
 };
